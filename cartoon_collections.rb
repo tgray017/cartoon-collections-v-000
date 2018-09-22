a4dde72fdf5ef8560bc944b6b1026c84199c9b88
tgray017 <tgray017@gmail.com>
2018-09-22 20:05:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf| 
+  dwarves.each_with_index do |dwarf, index| 
+    number = index + 1
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
